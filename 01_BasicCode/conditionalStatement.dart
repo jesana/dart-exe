f47b73f02df6078bfeb84c8b9f297cc4ee023811
jesana <dev.jesana@gmail.com>
2021-02-23 02:28:19 +0530

    conditionalStatement file is created

---
+++ 01_BasicCode/conditionalStatement.dart
@@ -0,0 +1,13 @@
+void main() {
+  int number = 103;
+
+  print("number = $number");
+
+  if (number % 2 == 0) {
+    print('Variable is Even');
+  } else if (number % 2 == 1) {
+    print('Variable is Odd');
+  } else {
+    print('Variable is Confused');
+  }
+}
